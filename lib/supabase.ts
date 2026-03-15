cb5cbee563e34cb11191f66d96c56001f4b2dd6f
FAEAsu <nathan.reber.pro@gmail.com>
2026-03-15 09:23:26 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = "https://lbuceegfaxgtsipdbclo.supabase.co"
-const supabaseAnonKey = "TA_CLE_PUBLISHABLE_OU_ANON_ICI"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxidWVjZWdmYXhndHNpcGRiY2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0ODQxNDQsImV4cCI6MjA4OTA2MDE0NH0.TX36DoUM4dgFVbMrOU-4iKrwbTKg-u0inGaSg08zTWc"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
